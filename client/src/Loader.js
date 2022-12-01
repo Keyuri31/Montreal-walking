238b869757c8570d523f9db14567a468100102e2
“Keyuri31” <keyuripatel95@gmail.com>
2022-12-01 16:07:30 -0500

    Keyuri - without stylingaaa

---
+++ client/src/Loader.js
@@ -0,0 +1,22 @@
+import { Audio } from 'react-loader-spinner';
+
+const Loader = () => {
+    return (
+        <>
+        <Audio
+  height="80"
+  width="80"
+  radius="9"
+  display="flex"
+  justify-content="center"
+  align-items="center"
+  color="#480987"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+        </>
+    );
+}
+ 
+export default Loader;
